5f813a127641140a820b84c2b52a5c91b426d10a
Elionora <78257077+Elionora7@users.noreply.github.com>
2022-07-17 00:45:23 +1000

    update to a better solution

--- src/Color.js
+++ src/Color.js
@@ -1,31 +1,21 @@
 import React,{Component} from "react";
 import  "./Color.css";
-import { choice } from "./helpers";
 
 class Color extends Component {
-   constructor(props){
-    super(props);
-    this.state = {color: choice(this.props.colors)  };
-    this.onHandleClick = this.onHandleClick.bind(this);
-  }
-
-  onHandleClick(){
-     this.pickColor();
-  }
-
-  pickColor(){
-    let newColor ;
-    do {
-        newColor = choice(this.props.colors)
-    } while (newColor === this.state.color);
-    this.setState({ color: newColor });
-  }
-  render(){
-    return (<div className='color' style={{backgroundColor: this.state.color}}
-    onClick={this.onHandleClick}></div>);
-  }
-  
-
+    constructor(props) {
+        super(props);
+        this.handleChangeColor = this.handleChangeColor.bind(this);
+    }
+ 
+    handleChangeColor() {
+        this.props.changeColor(this.props.color, this.props.value);
+    }
+ 
+    render() {
+        return (
+            <div className='color' style={{backgroundColor: this.props.color}}
+            onClick={this.handleChangeColor}></div>);
+        
+    }
 }
-
 export default Color;
--- src/ColorContainer.js
+++ src/ColorContainer.js
@@ -1,22 +1,49 @@
 import React,{Component} from "react";
 import Color from "./Color";
 import  "./ColorContainer.css";
+import { choice } from "./helpers";
+
 class ColorContainer extends Component {
    static defaultProps = {
      numBoxes: 18,
-     allColors: ["purple", "magenta", "violet", "pink","green","yellow"],
-     index:0
+     colors: ["purple", "magenta", "violet", "pink","green","yellow"]
     };
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            boxes: Array.from({length: this.props.numBoxes}, 
+                () => choice(this.props.colors)) 
+        }
+        this.changeColor = this.changeColor.bind(this);
+    }
+ 
+    changeColor(currentColor, key) {
+        let newColor;
+        do {
+            newColor = choice(this.props.colors);
+ 
+        } while (currentColor === newColor)
+ 
+        let newBoxes = [...this.state.boxes];
+        newBoxes[key] = newColor;
+ 
+        this.setState({ boxes: newBoxes});
+    }
+ 
+    render() {
+        return (
+            <div className='BoxContainer'>
+                {
+                    this.state.boxes.map((color, index) => {
+                        return <Color color={color} key={index} value={index}
+                                          changeColor={this.changeColor}/>
+                    })
+                }
+            </div>
+        )
+    }
   
-  render(){
-    const boxes = Array.from({length: this.props.numBoxes}).map(
-        () => (<Color key={this.props.index + 1} colors= {this.props.allColors} />
-    ));
-
-    return <div className='BoxContainer'>{boxes}</div>;
-    
-  }
 
 
 }
